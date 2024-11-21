c8eb68f876aa343c1c79e7ffa82e320a26fc2643
yang-dain <ydi824@yonsei.ac.kr>
2024-11-21 16:01:13 +0900

    table 수정

--- webapp/models.py
+++ webapp/models.py
@@ -11,6 +11,7 @@ class Custom_user(models.Model):
     pw_qust = models.IntegerField(default=0)
     pw_ans = models.CharField(max_length=10, blank=False, null=False)
     name = models.CharField(max_length=10, blank=False, null=False)
+    join_date = models.DateField(auto_now_add=True)
 
 class Event(models.Model):
     user = models.ForeignKey(Custom_user, on_delete=models.CASCADE)
@@ -19,6 +20,7 @@ class Event(models.Model):
     start_date = models.DateField(blank=True, null=True)
     end_date =  models.DateField(blank=False, null=False)
     memo = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
 
 class School_Event(models.Model):
     id = models.AutoField(primary_key=True)
