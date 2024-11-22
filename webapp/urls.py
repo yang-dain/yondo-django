47796a26bcd46f7d7254f060e7650a81723458fe
jeongsohyang <67493588+jeongsohyang@users.noreply.github.com>
2024-11-23 02:50:12 +0900

    로그인, 회원가입 추가
    로그인, 회원가입 추가

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 
     path('update/', views.update_schedule_view, name='update_schedule'), #학사일정 업데이트
     path('main/', views.main, name='main'), #메인화면
+    path('login/', views.login, name='login'), #로그인
+    path('signup/', views.signup, name='signup'), #회원가입
+]
 ]
